2c5441b1eb9e005b934f6dc6d330693009983f0c
Alif Jawad Mahi <mahi.alifjawad@gmail.com>
2025-12-02 00:09:35 +0600

    feat: Implement `type` builtin command for shell
    This will implement the builtin command `type`. It checks whether a command is a builtin, an executable file, or unrecognized.

--- app/main.go
+++ app/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"golang.org/x/exp/slices"
 	"os"
 	"strings"
 )
 
 func main() {
+	builtInCommands := []string{"echo", "exit", "type"}
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
@@ -19,12 +22,19 @@ func main() {
 		}
 
 		args := strings.Split(input, " ")
+		command := strings.TrimSpace(args[0])
 
-		switch args[0] {
-		case "exit\n":
+		switch command {
+		case "exit":
 			os.Exit(0)
 		case "echo":
 			fmt.Println(input[5 : len(input)-1])
+		case "type":
+			if slices.Contains(builtInCommands, strings.TrimSpace(args[1])) {
+				fmt.Println(strings.TrimSpace(args[1]), "is a shell builtin")
+			} else {
+				fmt.Println(strings.TrimSpace(args[1]) + ": command not found")
+			}
 		default:
 			fmt.Println(input[:len(input)-1] + ": command not found")
 		}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/codecrafters-io/shell-starter-go
 
 go 1.24.0
+
+require golang.org/x/exp v0.0.0-20251125195548-87e1e737ad39 // indirect
