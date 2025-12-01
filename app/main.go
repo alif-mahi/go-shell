6bf1dc3f57ecfabac5f5b6e9195e68b684ddad81
Alif Jawad Mahi <mahi.alifjawad@gmail.com>
2025-12-01 22:42:20 +0600

    feat: Implement exit
    This implements the `exit` built-in command which will terminate the shell immediately.

--- app/main.go
+++ app/main.go
@@ -7,20 +7,19 @@ import (
 )
 
 func main() {
-	i := 0
-	for i == 0 {
+	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
+		// Wait for user input
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
-
+			fmt.Fprintf(os.Stderr, "Error reading stdin: %v\n", err)
 			os.Exit(1)
-
 		}
 
+		if command == "exit\n" {
+			os.Exit(0)
+		}
 		fmt.Println(command[:len(command)-1] + ": command not found")
-
 	}
 }
